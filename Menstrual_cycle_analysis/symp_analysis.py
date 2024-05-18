783c1f3e17bcb43c3820b0daedcca3213990ae1d
Yini Chen <chenyini426@gmail.com>
2024-05-18 21:27:23 +0200

    symptom probabilities visual added
    Signed-off-by: Yini Chen <chenyini426@gmail.com>

--- Menstrual_cycle_analysis/symp_analysis.py
+++ Menstrual_cycle_analysis/symp_analysis.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-nr_of_cycle = [0, 1, 2, 3, 4, 5, 6]
-
 symp_file = pd.read_excel("Menstration_symp.xlsx")
 
 df = pd.DataFrame(symp_file)
 
-df.plot(x='Cycle', y=['Cramps','Tender_breasts','Low_energy','Headache'], kind='bar')
+df.plot(x='Cycle', y=['Cramps','Tender_breasts','Low_energy','Headache', 'Abdominal_pain','Nausea'], kind='bar')
+plt.xlabel('Cycle in days')
+plt.ylabel('1=positive, 0=negative')
+plt.title('Recurrence of symptoms')
+plt.legend()
 plt.show()
 
 def calculate_symps_probability(symptom_list):
@@ -25,8 +27,15 @@ def calculate_symps_probability(symptom_list):
 symp_list = df.columns.tolist()[2:] 
 print(symp_list)
 
+symp_probability_1 = []
 for i in symp_list:
     symp_probability = calculate_symps_probability(df[i])
+    symp_probability_1.append(symp_probability)
     print('Probability of getting', i, 'during', len(df[i]), 'days/7 cycles is', round(symp_probability,3)*100, '%')
 
+plt.bar(symp_list, symp_probability_1)
+plt.xlabel('Symptoms')
+plt.ylabel('Probability')
+plt.title('Probabilities of symptom occurrence')
+plt.show()
 
