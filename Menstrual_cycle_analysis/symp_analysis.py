e6d694353e2c09669b1cf111c471d4e626e81dc5
Yini Chen <chenyini426@gmail.com>
2024-05-19 13:27:59 +0200

    symp_analysis code updated, column days in cycle added, new visualization added
    Signed-off-by: Yini Chen <chenyini426@gmail.com>

--- Menstrual_cycle_analysis/symp_analysis.py
+++ Menstrual_cycle_analysis/symp_analysis.py
@@ -7,13 +7,34 @@ symp_file = pd.read_excel("Menstration_symp.xlsx")
 
 df = pd.DataFrame(symp_file)
 
-df.plot(x='Cycle', y=['Cramps','Tender_breasts','Low_energy','Headache', 'Abdominal_pain','Nausea'], kind='bar')
-plt.xlabel('Cycle in days')
-plt.ylabel('1=positive, 0=negative')
+#Adding days_in_cycle column into DataFrame
+cycles = df['Cycle'].tolist()
+day = 1
+#Create an empty list to store the day counts for each cycle
+cycle_days = []  
+prev_cycle = cycles[0]
+
+for cycle in cycles:
+    if prev_cycle == cycle:
+        cycle_days.append(day)
+    else:
+        day = 1
+        prev_cycle = cycle
+        cycle_days.append(day)
+    day += 1
+
+df['Days_in_cycles'] = pd.DataFrame(cycle_days)
+
+
+#Visualization of recurrence of symptoms
+df.plot(x='Days_in_cycles', y=['Cramps','Tender_breasts','Low_energy','Headache', 'Abdominal_pain','Nausea'], kind='bar')
+plt.xlabel('Day in each cycle, total cycles = 7')
+plt.ylabel('1=YES, 0=NO')
 plt.title('Recurrence of symptoms')
 plt.legend()
 plt.show()
 
+#Calculate probabilities of symptom occurrence
 def calculate_symps_probability(symptom_list):
     positive_symp = []
 
@@ -33,9 +54,16 @@ for i in symp_list:
     symp_probability_1.append(symp_probability)
     print('Probability of getting', i, 'during', len(df[i]), 'days/7 cycles is', round(symp_probability,3)*100, '%')
 
+#Visualization of probabilities of symptom occurence
 plt.bar(symp_list, symp_probability_1)
 plt.xlabel('Symptoms')
 plt.ylabel('Probability')
 plt.title('Probabilities of symptom occurrence')
 plt.show()
+
+Menstration_symp_1 = 'Menstration_symp_1.xlsx'
+df.to_excel(Menstration_symp_1)
+
+
+
 
