a7e67177033eb626fcee624b3bbe25e4c356b54b
Anders G Vestengen <a.g.vestengen@fys.uio.no>
2025-02-03 18:29:48 +0100

    Hide database credentials

--- Case/api.py
+++ Case/api.py
@@ -1,17 +1,18 @@
 import psycopg2
 import requests
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Connection parameters
-
-
 db_params = {
-    'dbname': 'postgres',
-    'user': 'postgres',
-    'password': '0426',
-    'host': 'localhost',
-    'port': '6969'
+    'dbname': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT')
 }
-
 
 try:
     # Establish connection
